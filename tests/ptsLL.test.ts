16871caf46b15f7691d183aa2b6deee7870395bd
Michael James <micrjames921@gmail.com>
2024-10-15 14:39:40 -0700

    created 'Point' interface; completed tests for 'Point' 'Having its values added to'

--- tests/ptsLL.test.ts
+++ tests/ptsLL.test.ts
@@ -1,35 +1,93 @@
 import { YAll } from "../YAll";
+import { IPoint } from "./IPoint";
 
 describe("A list of Points", () => {
-   interface Point {
-	   x: number;
-	   y: number;
-   };
-   let p1: Point;
-   let p2: Point;
-   let p3: Point;
-   let yall: YAll<Point>;
+   let pts: IPoint[];
+   let yall: YAll<IPoint>;
+   let listSize: number;
    beforeAll(() => {
 	  yall = new YAll(); 
-	  p1 = {x: 0, y: 0};
-	  yall.push_back(p1);
-	  p2 = {x: 1, y: 0};
-	  yall.push_back(p2);
-	  p3 = {x: 0, y: 1};
-	  yall.push_back(p3);
+	  pts = [
+		 {x: 0, y: 0},
+		 {x: 1, y: 0},
+		 {x: 0, y: 1}
+	  ];
+
+	  listSize = 0;
    });
-   test("Should exist.", () => {
-	  expect(yall).toBeDefined();
+   describe("Created", () => {
+	  test("Should exist.", () => {
+		 expect(yall).toBeDefined();
+	  });
+	  test("Should be empty.", () => {
+		 const yallIsEmpty = yall.empty;
+		 expect(yallIsEmpty).toBeTruthy();
+	  });
    });
-   test("Should have a size of 3.", () => {
-	  const ptYAllSize = yall.size;
-	  expect(ptYAllSize).toBe(3);
-   });
-   test("Should have a size of 3.", () => {
-	  let pointYall = yall.toString();
-	  console.log(pointYall);
-	  yall.reverse();
-	  pointYall = yall.toString();
-	  console.log(pointYall);
+   describe("Having its values added to", () => {
+	  let appendedValue: IPoint;
+	  beforeAll(() => {
+		 appendedValue = pts[0];
+	 	 yall.push_back(appendedValue);
+		 console.log(yall.toString());
+		 listSize++;
+	  });
+	  describe("Appending a value", () => {
+		 test("Should have one value in the linked list.", () => {
+			const yallSize = yall.size;
+			expect(yallSize).toBe(listSize);
+		 });
+		 test("Should give index 0 as the index of the appended value.", () => {
+			const idx = yall.idx_at(appendedValue);
+			expect(idx).toBe(0);
+		 });
+	  });
+	  describe("Prepending a value", () => {
+		 let prependedValue: IPoint;
+		 beforeAll(() => {
+			prependedValue = pts[1];
+			yall.push_front(prependedValue);
+			console.log(yall.toString());
+			listSize++;
+		 });
+		 test("Should have two values in the linked list.", () => {
+			const yallSize = yall.size;
+			expect(yallSize).toBe(listSize);
+		 });
+		 test("Should give index 0 as the index of the prepended value.", () => {
+			const idx = yall.idx_at(prependedValue);
+			console.log(idx);
+			expect(idx).toBe(0);
+		 });
+	  });
+	  describe("inserting a value", () => {
+		 let insertedValue: IPoint;
+		 let insertionIdx: number;
+		 beforeAll(() => {
+			insertedValue = pts[2];
+			insertionIdx = 2;
+			yall.insert(insertedValue, insertionIdx);
+			console.log(yall.toString());
+			listSize++;
+		 });
+		 test("Should throw an error if we insert a value at an index  of -1.", () => {
+			expect(() => {
+			   yall.insert({x: -1, y: -1}, -1);
+			}).toThrow("Out of Bounds.");
+		 });
+		 test("Should throw an error if we insert a value at an index  outside the linked list.", () => {
+			expect(() => {
+			   yall.insert({x: -1, y: -1}, yall.size+1);
+			}).toThrow("Out of Bounds.");
+		 });
+		 test("Should have three values in the linked list.", () => {
+			const yallSize = yall.size;
+			expect(yallSize).toBe(listSize);
+		 });
+		 test(`Should give index ${insertionIdx} as the index of the inserted value.`, () => {
+			const idx = yall.idx_at(insertedValue);
+			expect(idx).toBe(insertionIdx);
+		 });
+	  });
    });
 });
